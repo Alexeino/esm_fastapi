6f44a02e2dd68074603b4bed3c2b1d3885045cbc
Manish <manish.na@kpn.com>
2023-11-23 13:18:50 +0530

    Added UserRole class for seamless role authentication

--- api/v1/product/routes.py
+++ api/v1/product/routes.py
@@ -1,30 +1,20 @@
 from fastapi import APIRouter, Depends
-from settings.Security import decode_token
 from sqlalchemy.orm import Session
 from schemas.products import ProductSchema
 from db.session import get_db
 from db.models.product import Product
-from fastapi.exceptions import HTTPException
-from fastapi import status
 from schemas.base_schema import create_base_schema
 from schemas.products import ProductResponseSchema
 from typing import List
+from api.v1.user.views import UserRole
 
 product_router = APIRouter()
 
 
-@product_router.post("/add")
-async def create_product(
-    product: ProductSchema,
-    db: Session = Depends(get_db),
-    current_user: dict = Depends(decode_token),
-):
-    role = current_user.get("role")
-    if role not in ("STAFF", "ADMIN"):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You don't have authorization to perform this action",
-        )
+@product_router.post(
+    "/add", dependencies=[Depends(UserRole(allowed_roles=["STAFF", "ADMIN"]))]
+)
+async def create_product(product: ProductSchema, db: Session = Depends(get_db)):
     product = Product.create(db, **product.model_dump())
 
     Response = create_base_schema(
@@ -38,3 +28,12 @@ async def create_product(
 async def list_products(db: Session = Depends(get_db)):
     products = Product.all(db=db)
     return products
+
+
+@product_router.put(
+    "/update", dependencies=[Depends(UserRole(allowed_roles=["STAFF", "ADMIN"]))]
+)
+async def update_product(product: ProductSchema, db: Session = Depends(get_db)):
+    pass
+
+    # TODO: logic to update product
--- api/v1/user/views.py
+++ api/v1/user/views.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from db.models.User import User, UserManager
 from fastapi import status, exceptions
+from settings.Security import decode_token
 
 
 def get_user_by_email(email: str, db: Session = Depends(get_db)):
@@ -22,3 +23,17 @@ def authenticate_user(email: str, password: str, db: Session):
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Password Incorrect"
         )
     return user
+
+
+class UserRole:
+    def __init__(self, allowed_roles: list[str] = ["STAFF"]):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, current_user: dict = Depends(decode_token)):
+        role = current_user.get("role")
+        if role not in self.allowed_roles:
+            raise exceptions.HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You don't have authorization to perform this action",
+            )
+        return current_user
